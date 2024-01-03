15c92e809bf134a288ac70f9befe37b06046fd80
Joy <joy.logs@gmail.com>
2024-01-03 22:12:50 +0530

    list page completed with dynamic data

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Joy Banerjee on 03/01/24.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    
+    var id: Int
+    var name: String
+    var state: String
+    var park: String
+    var description: String
+    
+    var imageName: String
+    
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,49 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Joy Banerjee on 03/01/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+//struct Turtle_Rock: PreviewProvider {
+//    static var previews: some View {
+//        LandmarkRow(landmark: landmarks[0])
+//    }
+//}
+//
+//
+//struct Silver_Salmon_Creek: PreviewProvider {
+//    static var previews: some View {
+//        LandmarkRow(landmark: landmarks[1])
+//    }
+//}
+
+struct LandMarkRow_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+        }
+    }
+}
