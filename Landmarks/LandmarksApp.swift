5c4200aa3511e6014144a3dbfa20eba63ffd035d
Joy <joy.logs@gmail.com>
2024-01-03 20:00:55 +0530

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Joy Banerjee on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
