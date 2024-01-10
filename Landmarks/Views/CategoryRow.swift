1d3fd97f608a4c2f0d78895b8542038e0ff4d39e
Joy <joy.logs@gmail.com>
2024-01-10 23:29:40 +0530

    Added ScrollView data

--- Landmarks/Views/CategoryRow.swift
+++ Landmarks/Views/CategoryRow.swift
@@ -22,7 +22,8 @@ struct CategoryRow: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        Text(landmark.name)
+//                        Text(landmark.name)
+                        CategoryItem(landmark: landmark)
                     }
                 }
             }
