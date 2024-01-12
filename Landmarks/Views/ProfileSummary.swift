0a2f4d41a558770ea2eca574a1b549da899ca450
Joy <joy.logs@gmail.com>
2024-01-12 23:59:45 +0530

    added Grouping

--- Landmarks/Views/ProfileSummary.swift
+++ Landmarks/Views/ProfileSummary.swift
@@ -14,7 +14,9 @@ struct ProfileSummary: View {
 //        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         ScrollView {
             VStack {
-                Text()
+                Text("Notifications: ")
+                Text("")
+                
             }
         }
     }
