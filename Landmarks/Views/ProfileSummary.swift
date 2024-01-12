d13da649797a2996798e90ae72964aa5a0624f0b
Joy <joy.logs@gmail.com>
2024-01-12 23:57:02 +0530

    added Profile Summary

---
+++ Landmarks/Views/ProfileSummary.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Joy Banerjee on 12/01/24.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    
+    
+    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text()
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary()
+    }
+}
