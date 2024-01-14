d48479f06d44a68679ecbebc8ba211fee62cb01d
Joy <joy.logs@gmail.com>
2024-01-14 23:53:22 +0530

    Added date component to Profile Editor

--- Landmarks/Views/ProfileEditor.swift
+++ Landmarks/Views/ProfileEditor.swift
@@ -11,6 +11,12 @@ struct ProfileEditor: View {
     
     @Binding var profile: Profile
     
+    var dateRange: ClosedRange<Date> {
+            let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+            let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+            return min...max
+        }
+    
     var body: some View {
         List {
             HStack {
@@ -28,6 +34,10 @@ struct ProfileEditor: View {
             ForEach(Profile.Season.allCases) { season in
                 Text(season.rawValue).tag(season)
             }
+        }
+        
+        DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date ) {
+            Text("Goal Date")
         }
     }
 }
