4355eaf7f9567bc058a228997a1e2d9069931061
Joy <joy.logs@gmail.com>
2024-01-15 23:53:57 +0530

    removed redundant list which was showing a split view

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
     
     
     var body: some View {
-        LandmarkList()
+//        LandmarkList()
         
         TabView(selection: $tab) {
             CategoryHome()
