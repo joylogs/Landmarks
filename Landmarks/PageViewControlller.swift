f580a369b47b51272c0f25eef215ddd1cc297531
Joy <joy.logs@gmail.com>
2024-02-07 23:53:06 +0530

    wip:adding PageViewController

--- Landmarks/PageViewControlller.swift
+++ Landmarks/PageViewControlller.swift
@@ -6,9 +6,23 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     
-    
     var pages: [Page]
+    
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers([UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
 }
