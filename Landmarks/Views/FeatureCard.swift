5eaa38a74244967333a00af4f5cf78676b4750c7
Joy <joy.logs@gmail.com>
2024-02-08 23:44:49 +0530

    adding feature card view

--- Landmarks/Views/FeatureCard.swift
+++ Landmarks/Views/FeatureCard.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct FeatureCard: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        landmark.featureImage?
+            .resizable()
     }
 }
 
 struct FeatureCard_Previews: PreviewProvider {
     static var previews: some View {
-        FeatureCard()
+        FeatureCard(landmark: ModelData().features[0])
+            .aspectRatio(3 / 2 , contentMode: .fit)
     }
 }
