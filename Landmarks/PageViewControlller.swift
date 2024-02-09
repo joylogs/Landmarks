59c9f71970865ce84ca114fdaa80c4e97a8b83a6
Joy <joy.logs@gmail.com>
2024-02-09 23:22:28 +0530

    adding co-ordinator

--- Landmarks/PageViewControlller.swift
+++ Landmarks/PageViewControlller.swift
@@ -10,19 +10,47 @@ import UIKit
 import SwiftUI
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
-    
     var pages: [Page]
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
     }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            
+        }
+        
+        
+        var parent: PageViewController
+        var controllers = [PageViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            self.parent = pageViewController
+            self.controllers = self.parent.pages.map {
+                UIHostingController(rootView: $0)
+            }
+        }
+    }
+
 }
+
+
